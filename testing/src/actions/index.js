7085fb63fbdbe8ce953e563d91d6b3244e793e35
zeatful <zeatful@users.noreply.github.com>
2018-07-21 23:07:06 -0400

    create auth reducer and actions

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export function saveComment(comment){
     return {
@@ -14,4 +14,11 @@ export function fetchComments(){
         type: FETCH_COMMENTS,
         payload: response
     };
+}
+
+export function changeAuth(isLoggedIn){
+    return {
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
+    };    
 }
