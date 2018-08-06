04eaac613177b90110e557ced7d543cdb75deac8
zeatful <zeatful@users.noreply.github.com>
2018-08-06 00:10:53 -0400

    updated gitignore to avoid loading the config.js file

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,3 +1,25 @@
+const User = require('../models/user');
+
 exports.signup = function (req, res, next){
-    res.send({ success: 'true' });
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // check if user already exists for the email
+    User.findOne({email}, (err, user) => {
+        if(err){
+            return next(err);
+        } else if(user){
+            // unprocessable entity
+            return res.status(422).send({error: 'Email is already in use!'});
+        } else {
+            const user = new User({ email, password});
+            user.save(err => {
+                if(err){ 
+                    return next(err);
+                } else {
+                    res.json({ success: true});
+                }                
+            });
+        }
+    });
 };
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
-    email: { type: String, unique: true, lowercase: true},
-    password: String
+    email: { type: String, required: true, unique: true, lowercase: true},
+    password: { type: String, required: true }
 });
 
-const User = mongoose.model('user', userSchema);
+// On Save Hook, encrypt password
+userSchema.pre('save', next => {
+    const user = this;
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err) { return next(err);}
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if(err) { return next(err);}
+            user.password = hash;
+            next();
+        });
+    });
+});
 
-export default User;
+const ModelClass = mongoose.model('user', userSchema);
+
+module.exports = ModelClass;
