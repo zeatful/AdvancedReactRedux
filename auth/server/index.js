777aa3cacdad780db19c97b09971caf1a78b07ff
zeatful <zeatful@users.noreply.github.com>
2018-08-18 14:11:11 -0400

    wire up requireAuth higher order component for login and then setup localstorage to perist auth information

--- auth/server/index.js
+++ auth/server/index.js
@@ -4,18 +4,20 @@ const bodyParser = require( 'body-parser');
 const morgan = require( 'morgan');
 const router = require('./router.js');
 const mongoose = require( 'mongoose');
+const cors = require('cors');
 
 const app = express();
 
 // App setup -  express middleware for each request
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
 mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
 
-// Server steup
-const port = process.env.PORT || 3000;
+// Server setup
+const port = process.env.PORT || 3001;
 const server = http.createServer(app);
 server.listen(port);
 console.log(`Server listening on ${port}`);
